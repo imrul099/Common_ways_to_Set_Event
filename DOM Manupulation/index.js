039214bbba8f68572a02b260fba3091783465b39
MD IMRUL MOLLA <imrulmolla99@gmail.com>
2024-02-01 22:27:02 +0600

    this the first commit

---
+++ DOM Manupulation/index.js
@@ -0,0 +1,29 @@
+// querySelectorAll
+
+const mainContainer = document.querySelectorAll('.main-container li');
+// console.log(mainContainer);
+for(li of mainContainer){
+    console.log(li);
+}
+
+// document.getElementsByTagName
+// document.getElementById
+// document.getElementsByClassName
+// document.querySelector
+// document.querySelectorAll
+
+
+
+// getAttribute......
+// setAttribute...
+// innerText..
+// innerHtml...
+// remove
+// add
+
+
+
+// childNodes
+// firstNode
+// lastChild
+// sibling 
---
+++ Problem Solving/index.js
@@ -0,0 +1,7 @@
+// ludu khelai 1 to 6 number .
+
+function getRandomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomNumber(2,3));
